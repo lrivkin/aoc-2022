452337e4409c495ebd76c79d8dedad59eabb1324
Leah Rivkin <lrivkin@atlassian.com>
2022-12-04 20:38:53 -0800

    golang boilerplate

---
+++ day18/day18.go
@@ -0,0 +1,11 @@
+package main
+
+func part1() int {
+	return 0
+}
+func part2() int {
+	return 0
+}
+func main() {
+
+}
--- utils/setup_boilerplate.py
+++ utils/setup_boilerplate.py
@@ -1,5 +1,5 @@
-import os
 from pathlib import Path
+import shutil
 
 p = Path(__file__).parent.resolve().joinpath("..")
 # p.joinpath("..")
@@ -8,3 +8,8 @@ for day in range(1, 26):
     p.joinpath(f'day{day}/test.txt').touch(exist_ok=True)
     p.joinpath(f'day{day}/input.txt').touch(exist_ok=True)
     p.joinpath(f'day{day}/day{day}.go').touch(exist_ok=True)
+
+template = p.joinpath("day5/day5.go")
+for day in range(6, 26):
+    new_day = p.joinpath(f'day{day}/day{day}.go')
+    shutil.copy(template, new_day)
